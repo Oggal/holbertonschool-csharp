2f974a5bdc39d5ec76c1038b96707a4e5b94dc96
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-08 18:48:26 -0500

    Task 9

---
+++ csharp-hello_world/9-print_specifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine(percent.ToString("P"));
+        Console.WriteLine(currency.ToString("C", new System.Globalization.CultureInfo("en-US")));
+	}
+}
