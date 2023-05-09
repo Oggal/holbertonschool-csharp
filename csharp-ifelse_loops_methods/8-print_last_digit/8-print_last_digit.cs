77cf2b087b09b2eddb6851dc62cd9dd22a6cbcb7
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-09 14:03:21 -0500

    Task 8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int res = Math.Abs(number % 10);
+        Console.Write(res);
+        return res;
+    }
+}
