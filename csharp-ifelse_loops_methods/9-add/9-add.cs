bd69c6cefc31cf8c0ba924f1899ff8c5d05e65a4
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-09 14:05:19 -0500

    Task 9

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int res = Math.Abs(number % 10);
+        Console.Write(res);
+        return res;
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
