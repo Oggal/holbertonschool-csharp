6d37c7cb609a7ec49a76b4e86b456079cd6633ab
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-09 10:16:27 -0500

    Task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class HexHex
+{
+    public static void Main(string[] args)
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            Console.Write("{0}{1}",i.ToString("00"),i !=99? ", ":"");
+        }
+    }
+}
