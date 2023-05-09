4719f4ad815eb728a1de05314aaaa24c05ba3957
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-09 09:46:01 -0500

    Task 2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class alphabet
+{
+
+    public static void Main(string[] args)
+    {
+        for(char i = 'a'; i <= 'z'; i++)
+        {
+            Console.Write(i);
+        }
+    }
+}
