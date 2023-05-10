6fe86ba3fd610644ad8705a470d43465a57f7569
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-10 18:10:02 -0500

    Task 12

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keys = myDict.Keys.ToList();
+        keys.Sort();
+        foreach(string key in keys)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        } 
+    }
+}
