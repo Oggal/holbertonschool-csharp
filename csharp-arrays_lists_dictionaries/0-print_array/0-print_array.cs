860a5b0d18388fe16e612b014489a6c949866fc8
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-10 08:37:48 -0500

    Task 0, Missing negative Case

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int[] res = new int[size];
+        for(int i = 0; i < size; i++)
+            Console.Write("{0}{1}",(res[i] = i), " ");
+        Console.WriteLine();
+        return res;
+    }
+} 
