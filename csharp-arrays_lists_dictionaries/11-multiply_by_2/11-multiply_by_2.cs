3b5eb289da74c2694a93d14285336a5dec8b6178
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-10 18:02:06 -0500

    Task 11

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        foreach(string key in myDict.Keys)
+            myDict[key] *= 2;
+        return myDict;
+    }
+
+        
+}
