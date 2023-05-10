1c400255ea661b6ff6a463da1fbb33c9f9dcada6
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-10 10:04:34 -0500

    Task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> res = new List<int>();
+        for( int i = 0; i < size; i++)
+        {
+            res.Add(i);
+            Console.Write("{1}{0}", i, i!=0?" ":"");
+        }
+        Console.WriteLine();
+        return res;
+    }
+}
