2d3489342a0895c7e39daaaa6a6297d50fe1b861
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-10 09:51:11 -0500

    This is not what Holberton Expects, I'm sure...

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -27,7 +27,7 @@ public class Array
 
     public static void Reverse(int[] array)
     {
-        int x = array?.Length;
+        int x = (array?.Length).GetValueOrDefault(0);
         for(int i = x - 1; i != -1; i--)
         {
             Console.Write("{0}{1}", array[i], i == 0 ? "" : " ");
