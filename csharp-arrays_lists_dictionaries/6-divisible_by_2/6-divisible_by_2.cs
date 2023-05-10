e7323af2b27f222cfd01de5c99299b781162ad9a
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-10 10:41:51 -0500

    Task 6

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+public class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> res = new List<bool>();
+        foreach(int i in myList)
+            res.Add(i%2==0);
+        return res;
+    }
+}
+
