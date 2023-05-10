bcbd185498b1b73d75778cae00ef755828a6c6c0
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-10 15:30:45 -0500

    Task 7

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        List<int> res = new List<int>();
+        for(int i = 0; i < myList.Length; i++)
+        {
+            if( i == index)
+                continue;
+            res.Add(myList[i]);
+        }
+    }
+
+}
