e7142368db4d64de8e5cb4708d311d54ce4e635e
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-11 15:47:02 -0500

    Task 2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> res = new List<int>(list1);
+        foreach(int i in list2)
+        {
+            if(!res.Contains(i))
+                res.Add(i);
+            else
+                res.Remove(i);
+        }
+        return res;
+    }
+}
