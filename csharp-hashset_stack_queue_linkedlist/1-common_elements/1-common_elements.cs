fa84e441ae970af211dfcc3293341d31e7115769
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-11 15:36:23 -0500

    Task 1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> res = new HashSet<int>();
+        foreach(int i in list1)
+        {
+            foreach(int j in list2)
+            {
+                if (i == j){
+                    res.Add(i);
+                    break;
+                }
+            }
+        }
+        return new List<int>(res);
+    }
+}
