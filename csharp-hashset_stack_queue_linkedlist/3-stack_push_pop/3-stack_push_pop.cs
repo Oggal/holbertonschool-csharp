f463c761187a2776a1900989263e9f093ad9da66
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-11 16:45:37 -0500

    Task 3 - BAD PRACTICE, NOT CLEAN

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+
+    //This is a terrible fucntion! It does 5 things and has a name that descirbes only one of them.
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        //Thing 1: Print Count
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        //Thing 2: Print top item
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        //Thing 3: Print if Item exisits
+        bool found = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, found);
+        //Thing 4: Pop Till Item
+        if (found)
+        {
+            while (aStack.Pop() != search)
+                continue;
+        }
+        //thing 5: Push new item
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
