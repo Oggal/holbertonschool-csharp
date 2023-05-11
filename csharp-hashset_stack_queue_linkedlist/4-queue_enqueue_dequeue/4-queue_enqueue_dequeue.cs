d5368376c72264a99bffb53b55ae6acb71784240
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-11 17:00:44 -0500

    Task 4

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        bool found = aQueue.Contains(search);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, found);
+        if (found)
+        {
+            while (aQueue.Dequeue() != search)
+                continue;
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
