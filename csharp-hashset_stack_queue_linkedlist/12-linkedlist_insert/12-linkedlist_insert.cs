c10d0994d5e4399286716383b6c520cbbdddcfe1
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-12 12:03:03 -0500

    Task 12

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> last = null;
+        foreach(LinkedListNode<int> node in myLList)
+        {
+            if(!last)
+            {
+                if(n>last.Value)
+                    return myLList.AddBefore(last,n);
+            }else{
+                if( n < node)
+                    return myLList.AddBefore(node, n);
+            }
+        }
+        return myLList.AddLast(n);
+    }
+}
