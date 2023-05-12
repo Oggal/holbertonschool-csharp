fca5b2720d102a2574c3f7f0991c506d40272f99
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-12 12:39:50 -0500

    Done

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -6,7 +6,9 @@ public class LList{
     public static void Delete(LinkedList<int> myLList, int index)
     {
         int? j = (new List<int>(myLList))[index];
-        if(j != null)
+        if(j != null && j != 0)
             myLList.Remove(j.Value);
+        if( j==0)
+            myLList.RemoveFirst();
     }
 }
