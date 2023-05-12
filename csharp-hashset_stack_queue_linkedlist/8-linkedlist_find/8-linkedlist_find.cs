7a171e3f95235ac32540ccbbee680925563937bb
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-12 11:25:02 -0500

    Task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int res = -1;
+        foreach(int i in myLList)
+        {
+            res++;
+            if(i == value)
+                return res;
+        }
+        return -1;
+    }
+}
