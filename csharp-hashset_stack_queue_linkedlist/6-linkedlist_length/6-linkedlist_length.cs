069580d80b06349d9f57bad3ea466ad359d5aab8
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-12 12:52:02 -0500

    6 fix

--- csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -7,7 +7,7 @@ public class LList
     public static int Length(LinkedList<int> myList)
     {
         int i =0;
-        foreach(int i in myList)
+        foreach(int j in myList)
         {
             i++;
         }
