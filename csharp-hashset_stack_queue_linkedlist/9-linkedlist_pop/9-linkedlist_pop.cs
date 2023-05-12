fc74154ff0165dcae27f3a34dfa04f42ca713095
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-12 12:51:03 -0500

    9 fix

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,6 +5,8 @@ public class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
+        if (myLList == null || myLList.Count == 0)
+            return 0;
         int i = myLList.First.Value;
         myLList.RemoveFirst();
         return i;
