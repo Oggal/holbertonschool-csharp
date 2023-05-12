c3d931ddd264c6f790617230bffefe009bc4c0e7
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-12 11:41:46 -0500

    Task 10

--- csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -6,7 +6,7 @@ public class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
-        if(n > myLList.Count || n < 0)
+        if(n >= myLList.Count || n < 0)
             return 0;
         return (new List<int>(myLList))[n];
     }
