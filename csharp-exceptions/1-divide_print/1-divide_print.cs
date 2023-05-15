1b3352722cd7a9a36e30ca9400e931552eca6b9f
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-15 18:19:08 -0500

    Task 1

--- csharp-exceptions/1-divide_print/1-divide_print.cs
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -10,7 +10,9 @@ public class Int
             res = a / b;
         }
         catch(Exception)
-        {}
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
         finally
         {
             Console.WriteLine("{0} / {1} = {2}", a, b, res);
