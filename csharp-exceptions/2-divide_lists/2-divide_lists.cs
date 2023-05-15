e524c2c7f7b03c792102ebd2349754a98e484f85
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-15 18:23:11 -0500

    There is no indication that this is the intended behavior

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -6,16 +6,18 @@ public class List
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
         List<int> res = new List<int>();
-        try
+        for( int i = 0; i < listLength; i++)
         {
-            for( int i = 0; i < listLength; i++)
+            try
             {
-                res.Add(div(list1[i], list2[i]));
+                
+                    res.Add(div(list1[i], list2[i]));
+                
             }
-        }
-        catch(ArgumentOutOfRangeException)
-        {
-            Console.WriteLine("Out of range");
+            catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
         }
         return res;
     }
