e9818935735f551d7ce350e55cd9732ad838133e
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-19 16:22:25 -0500

    Any example of what they want would help

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -14,13 +14,7 @@ namespace MyMath.Tests
         {
             int res = MyMath.Operations.Add(1,1);
             Assert.AreEqual(2,res);
-        }
-        [Test]
-        public void BadAddition()
-        {
-            int res = MyMath.Operations.Add(1.5,1.5);
-            Assert.AreEqual(1.5 + 1.5, res);
-
+            Assert.Pass();
         }
     }
 }
