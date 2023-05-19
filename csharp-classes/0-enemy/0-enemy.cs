4c2991347a69f46c54e5265503da12e3aca96a1e
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-19 11:27:13 -0500

    Correct Class definition

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -2,8 +2,15 @@
 
 namespace Enemys
 {
-    public class Zombie()
+    ///<summary>
+    /// Required documentation creates unneeded noise when reading code.
+    /// Writing clean self-documenting code is a better practice.
+    /// See the book "Clean Code", page 63, Mandated Comments.
+    ///
+    /// Furthermore, documenting an empty class is just noise.
+    ///</summary>
+    public class Zombie
     {
-        
+
     }
 }
