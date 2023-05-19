bf50e19a26bd5c06727a99204d74f059629698eb
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-19 11:37:32 -0500

    here we go again...

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>
+    /// Required documentation creates unneeded noise when reading code.
+    /// Writing clean self-documenting code is a better practice.
+    /// See the book "Clean Code", page 63, Mandated Comments.
+    ///
+    /// Furthermore, documenting an empty class is just noise.
+    ///</summary>
+    public class Zombie
+    {
+            /**
+        <summary>
+            int health
+        </summary>
+        */
+        public int health;
+
+        /**
+        <summary>
+            Default Constructor
+        </summary>
+        */
+        public Zombie()
+        {
+        }
+    }
+}
