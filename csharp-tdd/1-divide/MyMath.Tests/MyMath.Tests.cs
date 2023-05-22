a59d5833349a6bee61040ae0ccc97f59075afd62
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-21 20:26:35 -0500

    Add Null Matrix test

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -29,6 +29,9 @@ namespace MyMath.Tests
             int [,] testA = new int [0,0];
             Assert.IsNull(MyMath.Matrix.Divide(testA,2));
             Assert.IsNull(MyMath.Matrix.Divide(testmatrix, 0));
+            Assert.IsNull(MyMath.Matrix.Divide(null, 2));
         }
+
+        
     }
 }
