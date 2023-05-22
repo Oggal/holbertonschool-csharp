1195bc609a9517721793a8ba60d4e95a5c240c03
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-22 12:45:15 -0500

    Task 4

---
+++ csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Text
+{
+    public static class Str
+    {
+        public static int UniqueChar(string s)
+        {
+            if( s == null || s.Length == 0)
+                return -1;
+            int index = 0;
+            for(int i = 1; i < s.Length; i++)
+            {
+                if(index == s.Length)
+                    return -1;
+                if(s[index] == s[i] && !(index == i))
+                {
+                    index++;
+                    i = -1;
+                    continue;
+                }
+            }
+            
+            return index;
+        }
+    }
+}
