24a031211daa054c49bd6fbf1dbaa9bb99d4972f
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-21 20:42:46 -0500

    Correct Code

--- csharp-tdd/1-divide/MyMath/MyMath.cs
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -13,7 +13,7 @@
             if (matrix == null)
                 return null;
             
-            int[,] res = matrix;
+            int[,] res = new int[matrix.GetLength(0),matrix.GetLength(1)];
             for (int i = 0; i < matrix.GetLength(0); i++)
                 for (int j = 0; j < matrix.GetLength(1); j++)
                     res[i,j] = matrix[i,j]/num;
