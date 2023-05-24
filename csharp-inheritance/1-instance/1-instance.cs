85b1c6c4cef7d32d1afcb075fe441b297c935996
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-24 10:16:52 -0500

    Task 1

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary> Obj class </summary>
+public static class Obj
+{
+    
+    /// <summary> Checks if object is an instance of Array </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is  Array;
+    }
+
+}
+
