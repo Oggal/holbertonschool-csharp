f8c1880a26dd69ed898a72f761268d0a973ccd7f
Philip Taylor <Oggal@users.noreply.github.com>
2023-05-28 13:24:35 -0500

    Task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public static class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if ( vector.Length > 3 || vector.Length < 2)
+            return -1;
+
+        double sum = 0;
+        foreach (double i in vector)
+            sum += i * i;
+        return Math.Round(Math.Sqrt(sum), 2);
+    }
+}
