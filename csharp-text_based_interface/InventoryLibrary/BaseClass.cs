7f7b9bc774814686d2f9040cbc47946c0df778b5
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-31 10:43:49 -0500

    Task 0, inital Tests

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <summary> BaseClass class </summary>
+    public class BaseClass
+    {
+        /// <summary> BaseClass id </summary>
+        public string id { get;}
+
+        /// <summary> date created </summary>
+        public DateTime date_created { get;}
+
+        /// <summary> date updated </summary>
+        public DateTime date_updated { get;}
+    
+    
+            
+    }
+}
