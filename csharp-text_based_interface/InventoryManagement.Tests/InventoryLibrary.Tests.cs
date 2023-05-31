bb4d5afb55eab3f737c7edd9b5daeafac4d35c5f
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-05-31 12:52:05 -0500

    Task 1 & UnitTests

--- csharp-text_based_interface/InventoryManagement.Tests/InventoryLibrary.Tests.cs
+++ csharp-text_based_interface/InventoryManagement.Tests/InventoryLibrary.Tests.cs
@@ -7,10 +7,15 @@ namespace InventoryManagement.Tests
     public class Tests
     {
         BaseClass testCase;
+        Item testItem;
+        User testUser;
+
         [SetUp]
         public void Setup()
         {
             testCase = new BaseClass();
+            testItem = new Item("testItem");
+            testUser = new User("testUser");
         }
 
         [Test]
@@ -19,12 +24,51 @@ namespace InventoryManagement.Tests
             Assert.IsInstanceOf<string>(testCase.id);
             Assert.IsInstanceOf<DateTime>(testCase.date_created);
             Assert.IsInstanceOf<DateTime>(testCase.date_updated);
+
+            Assert.IsNotNull(testCase.id);
+            Assert.IsNotNull(testCase.date_created);
+            Assert.IsNotNull(testCase.date_updated);
         }
 
         [Test]
-        public void Test2()
+        public void ItemTest()
         {
-            Assert.Pass();
+            Assert.IsInstanceOf<string>(testItem.name);
+            Assert.IsInstanceOf<string>(testItem.description);
+            Assert.IsInstanceOf<float>(testItem.price);
+            Assert.IsInstanceOf<string[]>(testItem.tags);
+
+            Assert.IsNotNull(testItem.name);
+        }
+
+        [TestCase( new object[] {"dogs"}, ExpectedResult =  1)]
+        [TestCase( new object[] {"dogs", "cats"}, ExpectedResult =  2)]
+        [TestCase( new object[] {"dogs", "cats", "dogs"}, ExpectedResult =  2)]
+        [TestCase( new object[] {"dogs", "cats", "dogs", "cats"}, ExpectedResult =  2)]
+        [TestCase( new object[] {}, ExpectedResult =  0)]
+        []
+        public int TagCountTest(params string[] value)
+        {
+            Item LocalItem;
+            try
+            {
+                LocalItem = new Item("testItem", tags: value);
+                return LocalItem.tags.Length;
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+            
+            return -1;
+        }
+
+        [Test]
+        public void UserTest()
+        {
+            Assert.IsInstanceOf<string>(testUser.name);
+
+            Assert.IsNotNull(testUser.name);
         }
     }
 }
